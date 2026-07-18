05bdc6cfb6ca673e18ceef2897e3809c295c49f4
thomaskjohnson16 <thomaskjohnson16@gmail.com>
2026-07-18 05:02:04 -0400

    fix: add public contact configuration and remove placeholder values

---
+++ js/site-config.js
@@ -0,0 +1,7 @@
+window.ECS_SITE_CONFIG = Object.freeze({
+  publicContactEmail: "Contact@executiveculturesolutions.com",
+  intakeEmail: "Intake@executiveculturesolutions.com",
+  businessPhone: "(910) 315-0436",
+  chatbotEnabled: false,
+  chatbotEndpoint: ""
+});
